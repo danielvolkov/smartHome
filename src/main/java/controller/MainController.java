2aa5096bd3f143613b0b0291de9e1b722abc230e
danielvolkov <danielvolkov94@gmail.com>
2016-09-26 18:54:34 +0300

    adding login form

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Created by daniel on 26/09/16.
- */
+
 @Controller
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(){
         return "/index";
     }
+
 }
